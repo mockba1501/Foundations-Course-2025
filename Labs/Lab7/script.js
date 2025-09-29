e8b16d5b954d3de66c345c175aaba61667451cc0
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-09-29 20:14:11 +0300

    Lab7 Activity

---
+++ Labs/Lab7/script.js
@@ -0,0 +1,23 @@
+let firstName = "Ahmed";
+
+let newSentence = "Hello my name is " + firstName;
+
+//console.log(newSentence);
+
+// console.log("Hello I'm " + firstName + ", I am called Mohammed, call me Mohammed .... Mohammed kshdshfkjdshf ks Mohammed");
+
+console.log(`Hello I'm ${firstName}, I am called ${firstName}, call me ${firstName} 
+    .... ${firstName} kshdshfkjdshf ks ${firstName}`);
+
+// alert("Hi How are you!!")
+let num1;
+const num2 = 5;
+
+num1 = 10;
+
+let result = num1;
+console.log(result);
+
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
