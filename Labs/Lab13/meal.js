3ef380949b10d8888ef1440853479f8c15d0cd77
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-11-10 19:57:55 +0200

    Lab 13 Initial Files
    Fetching basics

---
+++ Labs/Lab13/meal.js
@@ -0,0 +1,24 @@
+//1- Link to get a random meal
+let randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+//2- Link to lookup a specific meal with an id
+//https://www.themealdb.com/api/json/v1/1/lookup.php?i=
+
+//3- Link to search for meals using a keyword
+//https://www.themealdb.com/api/json/v1/1/search.php?s=
+
+
+//async function getRadnomMeal() {
+const getRadnomMeal = async ()=> {
+   /* fetch(randomMealURL)
+    .then((item)=> item.json())
+    .then((data)=>console.log(data.meals[0]))
+    .catch((err)=>console.log("There was an issue in the fetching ",err));
+    */
+   const resp = await fetch(randomMealURL);
+   const data = await resp.json();
+   console.log(data.meals[0]);
+}
+
+
+getRadnomMeal();
