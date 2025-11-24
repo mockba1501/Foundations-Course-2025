eb7ea58a150fc4c036e00b215c4e04c32c883d4f
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-11-24 19:55:51 +0200

    Lab 16 Intro to Firebase
    Implemented SignUp functionality

---
+++ Labs/Lab16/auth.js
@@ -0,0 +1,25 @@
+//const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { firebaseConfig } from "./config.js";
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  export const signup = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      console.log(userCredential);
+      return userCredential;
+    }
+    catch(error){
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorCode, errorMessage);
+    }
+    
+  
+  }
---
+++ Labs/Lab16/logging.js
@@ -0,0 +1,32 @@
+import {signup} from './auth.js';
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const signupBtn = document.getElementById("signup");
+const signinBtn = document.getElementById("login");
+const messageDiv = document.getElementById("message");
+
+const showMessage = (msg, isError=false) => {
+    messageDiv.textContent = msg;
+    messageDiv.style.color = isError? 'red' : 'green';
+}
+
+signupBtn.addEventListener("click", async (e)=>{
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    //console.log("Sign Up Clicked!", email, password);
+    try{
+        const user = await signup(email, password);
+        showMessage("signup successful. Welcome ", user.email);
+        setTimeout(()=> {
+            window.location.ref = "app.html";
+        }, 1000)
+    }
+    catch (error){
+        showMessage(error.message, true);
+    }
+    
+    //console.log(user);
+})
